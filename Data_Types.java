13e0f0e20bf23d80d941de8e0c4809035d9a056d
jakariahossain24 <jakariahossain00011@gmail.com>
2026-01-18 13:05:27 +0600

    Data Types

---
+++ Data_Types.java
@@ -0,0 +1,18 @@
+public class Data_Types {
+    public static void main(String[] args) {
+        boolean b =true;
+        char c ='j';
+        short s =34;
+        int i =1275;
+        float f =10.5f;
+        double d =23.890;
+
+        System.out.println("Boolean= "+b);
+        System.out.println("char= "+c);
+        System.out.println("short= "+s);
+        System.out.println("int= "+i);
+        System.out.println("float= "+f);
+        System.out.println("double= "+d);
+
+    }
+}
