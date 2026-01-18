594751c09250af2156892695885f86da910b03ea
jakariahossain24 <jakariahossain00011@gmail.com>
2026-01-18 13:31:38 +0600

    Format Specifier

---
+++ Format_Specifier.java
@@ -0,0 +1,15 @@
+public class Format_Specifier {
+    public static void main(String[] args) {
+        int id = 101;
+        String title = "iphone15";
+        String price = "1895 euros";
+        String description = "perfect product with best image quality";
+        String category = "phone";
+
+        System.out.printf("id = %d\n",id);
+        System.out.printf("title = %s\n",title);
+        System.out.printf("price = %s\n",price);
+        System.out.printf("description = %s\n",description);
+        System.out.printf("category = %s\n",category);
+    }
+}
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,4 @@
+System.out.println("title = " + title);
+        // System.out.println("price = " + price);
+        // System.out.println("description = " + description);
+        // System.out.println("category = " + category);
