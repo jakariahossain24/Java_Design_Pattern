d8b9e15dad1236fea6076c54262f743e40779dd9
jakariahossain24 <jakariahossain00011@gmail.com>
2026-01-28 13:47:17 +0600

    Show also age

--- Features_OOP/Encapsulation.java
+++ Features_OOP/Encapsulation.java
@@ -8,11 +8,20 @@ public class Encapsulation {
     public void setName(String name){
         this.name=name;
     }
+    public int getAge(){
+        return age;
+    }
+    public void setAge(int age){
+        this.age=age;
+    }
 }
 class EncapsulationTest{
     public static void main(String[] args) {
         Encapsulation person=new Encapsulation();
+
         person.setName("Jakaria");
+        person.setAge(24);
         System.out.println(person.getName());
+        System.out.println(person.getAge());
     }
 }
